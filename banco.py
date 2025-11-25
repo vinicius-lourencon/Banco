b39722a1d99b3d11453316e955df9fd0e70a3c14
vinicius-lourencon <rimaria.depaula@gmail.com>
2025-11-25 13:48:04 -0300

    banco definido

--- banco.py
+++ banco.py
@@ -6,4 +6,21 @@ from conta_corrente import ContaCorrente
 from conta_poupanca import ContaPoupanca
 from excecoes import ContaNaoEncontradaError, ErroBanco
 
+class Banco:
+    """
+    Representa um banco simples responsável por gerenciar clientes e contas.
+
+    Esta classe oferece métodos para cadastrar clientes, abrir contas e realizar
+    operações como depósito, saque, transferência e emissão de extrato.
+    """
+
+    def __init__(self, nome: str) -> None:
+        """
+        Inicializa uma nova instância de Banco.
+
+        :nome: Nome do banco.
+        """
+        self._nome: str = nome
+        self._clientes: Dict[str, Cliente] = {}  # chave: CPF
+        self._contas: Dict[str, Conta] = {}      # chave: número da conta
 
